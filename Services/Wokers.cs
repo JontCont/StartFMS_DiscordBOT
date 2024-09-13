ef51637d439cdd1bd63f4aebe34d410e888c72da
ConteMA <52921053+JontCont@users.noreply.github.com>
2024-09-13 17:50:53 +0800

    Refactor program structure and add configuration support

---
+++ Services/Wokers.cs
@@ -0,0 +1,107 @@
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Discord.Interactions;
+using Microsoft.Extensions.Configuration;
+
+class Wokers
+{
+    // setup our fields we assign later
+    private readonly IConfiguration _config;
+    private DiscordSocketClient? _client;
+    private InteractionService? _commands;
+    private ulong _testGuildId;
+
+    public Wokers()
+    {
+        var _builder = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile(path: "config.json");
+
+        // build the configuration and assign to _config          
+        _config = _builder.Build();
+        _testGuildId = ulong.Parse(_config["TestGuildId"]);
+    }
+
+    public async Task RunAsync()
+    {
+        // call ConfigureServices to create the ServiceCollection/Provider for passing around the services
+        using (var services = ConfigureServices())
+        {
+            // get the client and assign to client 
+            // you get the services via GetRequiredService<T>
+            var client = services.GetRequiredService<DiscordSocketClient>();
+            var commands = services.GetRequiredService<InteractionService>();
+            _client = client;
+            _commands = commands;
+
+            // setup logging and the ready event
+            client.Log += LogAsync;
+            commands.Log += LogAsync;
+            client.Ready += ReadyAsync;
+
+            // this is where we get the Token value from the configuration file, and start the bot
+            await client.LoginAsync(TokenType.Bot, _config["Token"]);
+            await client.StartAsync();
+
+            // we get the CommandHandler class here and call the InitializeAsync method to start things up for the CommandHandler service
+            await services.GetRequiredService<CommandHandler>().InitializeAsync();
+
+            await Task.Delay(Timeout.Infinite);
+        }
+    }
+
+    private Task LogAsync(LogMessage log)
+    {
+        Console.WriteLine(log.ToString());
+        return Task.CompletedTask;
+    }
+
+    private async Task ReadyAsync()
+    {
+        if (IsDebug())
+        {
+            // this is where you put the id of the test discord guild
+            System.Console.WriteLine($"In debug mode, adding commands to {_testGuildId}...");
+            await _commands.RegisterCommandsToGuildAsync(_testGuildId);
+        }
+        else
+        {
+            // this method will add commands globally, but can take around an hour
+            await _commands.RegisterCommandsGloballyAsync(true);
+        }
+        Console.WriteLine($"Connected as -> [{_client.CurrentUser}] :)");
+    }
+
+    // this method handles the ServiceCollection creation/configuration, and builds out the service provider we can call on later
+    private ServiceProvider ConfigureServices()
+    {
+        // 這裡返回一個 ServiceProvider，稍後用於調用這些服務
+        // 我們可以在這裡添加我們可以訪問的類型，因此添加新的 using 語句：
+        return new ServiceCollection()
+            .AddSingleton(_config)
+            .AddSingleton<DiscordSocketClient>()
+            .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+            .AddSingleton<CommandHandler>()
+            .BuildServiceProvider();
+    }
+
+    static bool IsDebug()
+    {
+        var env = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+        Console.WriteLine($"Environment: {env}");
+        if (env == null)
+        {
+            return false;
+        }
+        if (env.ToLower() == "development")
+        {
+            return true;
+        }
+        return false;
+    }
+}
