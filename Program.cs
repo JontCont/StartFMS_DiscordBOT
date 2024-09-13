559fb1f355af5d8439a56bf633464e03a58c5ae2
ConteMA <52921053+JontCont@users.noreply.github.com>
2024-09-13 12:32:40 +0800

    initial

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Program
+{
+    private DiscordSocketClient _client;
+    private CommandService _commands;
+    private IServiceProvider _services;
+
+    public static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+    public async Task RunBotAsync()
+    {
+        _client = new DiscordSocketClient();
+        _commands = new CommandService();
+        _services = new ServiceCollection()
+            .AddSingleton(_client)
+            .AddSingleton(_commands)
+            .BuildServiceProvider();
+
+        _client.Log += Log;
+
+        await RegisterCommandsAsync();
+
+        await _client.LoginAsync(TokenType.Bot, "YOUR_BOT_TOKEN");
+
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage arg)
+    {
+        Console.WriteLine(arg);
+        return Task.CompletedTask;
+    }
+
+    public async Task RegisterCommandsAsync()
+    {
+        _client.MessageReceived += HandleCommandAsync;
+
+        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+    }
+
+    private async Task HandleCommandAsync(SocketMessage arg)
+    {
+        var message = arg as SocketUserMessage;
+        var context = new SocketCommandContext(_client, message);
+
+        if (message.Author.IsBot) return;
+
+        int argPos = 0;
+        if (message.HasStringPrefix("/", ref argPos))
+        {
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+            if (!result.IsSuccess)
+                Console.WriteLine(result.ErrorReason);
+        }
+    }
+}
+
+public class AddCommand : ModuleBase<SocketCommandContext>
+{
+    [Command("add")]
+    public async Task AddRole([Remainder] string key)
+    {
+        var user = Context.User as SocketGuildUser;
+        var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == key);
+
+        if (role != null)
+        {
+            await user.AddRoleAsync(role);
+            await ReplyAsync($"Added role {role.Name} to {user.Username}");
+        }
+        else
+        {
+            await ReplyAsync($"Role {key} not found");
+        }
+    }
+}
