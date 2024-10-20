4fbb343ba29277990db26e59fc6f478270517603
ConteMA <52921053+JontCont@users.noreply.github.com>
2024-10-20 18:45:55 +0800

    Add global using statements for Discord, Discord.Interactions, Newtonsoft.Json, and System.Net Rename GeneralCommands.cs to AdministratorCommands.cs and update class name and permissions Refactor GeneralCommands.cs to remove unused code and reorganize commands

---
+++ GlobalUsing.cs
@@ -0,0 +1,4 @@
+global using Discord;
+global using Discord.Interactions;
+global using Newtonsoft.Json;
+global using System.Net;
--- Modules/AdministratorCommands.cs
+++ Modules/AdministratorCommands.cs
@@ -1,37 +1,17 @@
-using System.Net;
-using Discord;
-using Discord.Interactions;
-using Newtonsoft.Json;
-
-class GeneralCommands : InteractionModuleBase<SocketInteractionContext>
+[RequireUserPermission(GuildPermission.Administrator)]
+class AdministratorCommands : InteractionModuleBase<SocketInteractionContext>
 {
     public required InteractionService Commands { get; set; }
     private CommandHandler _handler;
 
     // constructor injection is also a valid way to access the dependecies
-    public GeneralCommands(CommandHandler handler)
+    public AdministratorCommands(CommandHandler handler)
     {
         _handler = handler;
     }
 
-    [SlashCommand("回應", "隨便回應!!!")]
-    public async Task RespondAsync()
-    {
-        var embed = new EmbedBuilder()
-        .WithTitle("嵌入消息標題")
-        .WithDescription("這是一個嵌入消息的描述。")
-        .WithColor(Color.Blue)
-        .WithFooter(footer => footer.Text = "這是底部文字")
-        .WithTimestamp(DateTimeOffset.Now)
-        .Build();
-        await RespondAsync("這是範本", embeds: [embed], ephemeral: true);
-    }
-
-    // -- 語音 -- //
-
     // 產生語音頻道
-    [SlashCommand("創建語音", "建立語音頻道")]
-    [RequireUserPermission(GuildPermission.Administrator)]
+    [SlashCommand("create-voice", "建立語音頻道")]
     public async Task CreateVoiceChannel(string 頻道名稱, string? 指定分類 = null, string? 擁有身分組 = null)
     {
         var guild = Context.Guild;
@@ -60,8 +40,7 @@ class GeneralCommands : InteractionModuleBase<SocketInteractionContext>
     }
 
     // 語音頻道加入擁有身分組最大權限
-    [SlashCommand("語音最大權限", "語音頻道加入擁有身分組最大權限")]
-    [RequireUserPermission(GuildPermission.Administrator)]
+    [SlashCommand("add-voice-admin", "語音頻道加入擁有身分組最大權限")]
     public async Task AddVoiceChannelToRoleMaxPermission(string 頻道名稱, string 角色名稱)
     {
         var guild = Context.Guild;
@@ -82,8 +61,7 @@ class GeneralCommands : InteractionModuleBase<SocketInteractionContext>
     }
 
     // 語音頻道加入身分組
-    [SlashCommand("語音加入身分組", "語音頻道加入身分組")]
-    [RequireUserPermission(GuildPermission.Administrator)]
+    [SlashCommand("add-voice-role", "語音頻道加入身分組")]
     public async Task AddVoiceChannelToRole(string 頻道名稱, string 角色名稱)
     {
         var guild = Context.Guild;
@@ -104,8 +82,7 @@ class GeneralCommands : InteractionModuleBase<SocketInteractionContext>
     }
 
     // 修改語音頻道名稱
-    [SlashCommand("修改語音", "修改語音頻道名稱")]
-    [RequireUserPermission(GuildPermission.Administrator)]
+    [SlashCommand("modify-voice", "修改語音頻道名稱")]
     public async Task ModifyVoiceChannel(string 頻道名稱, string 新名稱)
     {
         var guild = Context.Guild;
@@ -120,8 +97,7 @@ class GeneralCommands : InteractionModuleBase<SocketInteractionContext>
     }
 
     // 刪除語音頻道
-    [SlashCommand("刪除語音", "刪除語音頻道")]
-    [RequireUserPermission(GuildPermission.Administrator)]
+    [SlashCommand("delete-voice", "刪除語音頻道")]
     public async Task DeleteVoiceChannel(string 頻道名稱)
     {
         var guild = Context.Guild;
@@ -135,11 +111,8 @@ class GeneralCommands : InteractionModuleBase<SocketInteractionContext>
         await RespondAsync($"已刪除語音頻道：{voiceChannel.Name}", embeds: [], ephemeral: true);
     }
 
-
-    //--  文字 -- //
     // 產生文字頻道
     [SlashCommand("create-text", "建立文字頻道")]
-    [RequireUserPermission(GuildPermission.Administrator)]
     public async Task CreateTextChannel(string 頻道名稱, string? 指定分類 = null, string? 擁有身分組 = null)
     {
         var guild = Context.Guild;
@@ -162,8 +135,7 @@ class GeneralCommands : InteractionModuleBase<SocketInteractionContext>
     }
 
     // 文字頻道加入擁有身分組最大權限
-    [SlashCommand("文字最大權限", "文字頻道加入擁有身分組最大權限")]
-    [RequireUserPermission(GuildPermission.Administrator)]
+    [SlashCommand("add-text-admin", "文字頻道加入擁有身分組最大權限")]
     public async Task AddTextChannelToRoleMaxPermission(string 頻道名稱, string 角色名稱)
     {
         var guild = Context.Guild;
@@ -184,8 +156,7 @@ class GeneralCommands : InteractionModuleBase<SocketInteractionContext>
     }
 
     // 文字頻道加入身分組
-    [SlashCommand("文字加入身分組", "文字頻道加入身分組")]
-    [RequireUserPermission(GuildPermission.Administrator)]
+    [SlashCommand("add-text-role", "文字頻道加入身分組")]
     public async Task AddTextChannelToRole(string 頻道名稱, string 角色名稱)
     {
         var guild = Context.Guild;
@@ -206,8 +177,7 @@ class GeneralCommands : InteractionModuleBase<SocketInteractionContext>
     }
 
     // 修改文字頻道名稱
-    [SlashCommand("修改文字頻道名稱", "修改文字頻道名稱")]
-    [RequireUserPermission(GuildPermission.Administrator)]
+    [SlashCommand("modify-text", "修改文字頻道名稱")]
     public async Task ModifyTextChannel(string 頻道名稱, string 新名稱)
     {
         var guild = Context.Guild;
@@ -222,7 +192,7 @@ class GeneralCommands : InteractionModuleBase<SocketInteractionContext>
     }
 
     // 刪除文字頻道
-    [SlashCommand("刪除文字頻道", "刪除文字頻道")]
+    [SlashCommand("delete-text", "刪除文字頻道")]
     [RequireUserPermission(GuildPermission.Administrator)]
     public async Task DeleteTextChannel(string 頻道名稱)
     {
@@ -237,8 +207,6 @@ class GeneralCommands : InteractionModuleBase<SocketInteractionContext>
         await RespondAsync($"已刪除文字頻道：{textChannel.Name}", embeds: [], ephemeral: true);
     }
 
-
-    //--  分類 -- //
     // 產生分類
     [SlashCommand("create-category", "建立分類")]
     [RequireUserPermission(GuildPermission.Administrator)]
@@ -281,8 +249,6 @@ class GeneralCommands : InteractionModuleBase<SocketInteractionContext>
         await RespondAsync($"已刪除分類：{category.Name}", embeds: [], ephemeral: true);
     }
 
-    // -- 角色 -- //
-
     // 產生角色
     [SlashCommand("create-role", "建立角色")]
     [RequireUserPermission(GuildPermission.Administrator)]
@@ -295,7 +261,6 @@ class GeneralCommands : InteractionModuleBase<SocketInteractionContext>
 
     // 修改角色名稱
     [SlashCommand("modify-role", "修改角色名稱")]
-    [RequireUserPermission(GuildPermission.Administrator)]
     public async Task ModifyRole(string roleName, string newRoleName)
     {
         var guild = Context.Guild;
@@ -311,7 +276,6 @@ class GeneralCommands : InteractionModuleBase<SocketInteractionContext>
 
     // 刪除角色
     [SlashCommand("delete-role", "刪除角色")]
-    [RequireUserPermission(GuildPermission.Administrator)]
     public async Task DeleteRole(string roleName)
     {
         var guild = Context.Guild;
@@ -323,102 +287,5 @@ class GeneralCommands : InteractionModuleBase<SocketInteractionContext>
         }
         await role.DeleteAsync();
         await RespondAsync($"已刪除角色：{role.Name}", embeds: [], ephemeral: true);
-    }
-
-
-    //抽身分組
-    [SlashCommand("抽職業", "抽職業搂")]
-    public async Task ReadomRole()
-    {
-        string[] relos = { "職業 : 鑑賞家", "職業 : 盜賊", "職業 : 賭徒", "職業 : 藝術家", "職業 : 設計師", "職業 : 藝人", "職業 : 法師", "職業 : 巫師", "職業 : 魔法師", "職業 : 劍士", "職業 : 咒術師", "職業 : 咒言師", "職業 : 無業戰士" };
-        Random random = new Random();
-        int index = random.Next(relos.Length);
-        await RespondAsync($"您抽到 [{relos[index]}] ", embeds: [], ephemeral: false);
-
-        var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == relos[index]);
-        if (role == null)
-        {
-            await createUserRole(relos[index]);
-        }
-        await JoinUserRole(relos[index]);
-    }
-
-
-
-    // 輸入指令者會加入身分組
-    public async Task JoinUserRole(string roleName)
-    {
-        var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == roleName);
-        var user = Context.User as IGuildUser;
-        if (user != null) await user.AddRoleAsync(role);
-    }
-
-    //創建身分組
-    public async Task createUserRole(string roleName)
-    {
-        var guild = Context.Guild;
-        Random random = new Random();
-        var role = await guild.CreateRoleAsync(roleName, color: new Color(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255)));
-    }
-
-    // 移除所有身分組
-    [SlashCommand("移除抽籤所有身分組", "移除所有身分組")]
-    [RequireUserPermission(GuildPermission.Administrator)]
-    public async Task RemoveAllRole()
-    {
-        string[] relos = { "職業 : 鑑賞家", "職業 : 盜賊", "職業 : 賭徒", "職業 : 藝術家", "職業 : 設計師", "職業 : 藝人", "職業 : 法師", "職業 : 巫師", "職業 : 魔法師", "職業 : 劍士", "職業 : 咒術師", "職業 : 咒言師", "職業 : 無業戰士" };
-        var guild = Context.Guild;
-        foreach (var role in guild.Roles)
-        {
-            if (relos.Contains(role.Name))
-            {
-                await role.DeleteAsync();
-            }
-        }
-        await RespondAsync($"已移除所有身分組", embeds: [], ephemeral: true);
-    }
-
-    // our first /command!
-    [SlashCommand("join", "加入")]
-    public async Task JoinTicket(string ticketId)
-    {
-        string apiUrl = $"http://localhost:3000/ticket/{ticketId}";
-        var client = new HttpClient();
-        var response = await client.GetAsync(apiUrl);
-        var content = await response.Content.ReadAsStringAsync();
-
-        if (response.StatusCode != HttpStatusCode.OK)
-        {
-            await RespondAsync("此票不存在", embeds: [], ephemeral: true);
-            return;
-        }
-
-        var ticket = JsonConvert.DeserializeObject<TicketArgs>(content);
-        if (ticket == null)
-        {
-            await RespondAsync("此票不存在", embeds: [], ephemeral: true);
-        }
-        else
-        {
-            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == ticket.role);
-
-            //如果身上有這個身分組就不用再加入
-            if (Context.User is IGuildUser user)
-            {
-                if (role == null)
-                {
-                    await createUserRole(ticket.role);
-                }
-
-                if (role != null && user.RoleIds.Contains(role.Id))
-                {
-                    await RespondAsync("您已經加入過了", embeds: [], ephemeral: true);
-                    return;
-                }
-
-                await JoinUserRole(ticket.role);
-                await RespondAsync($"您加入了 {ticket.role}", embeds: [], ephemeral: true);
-            }
-        }
     }
 }
